9c246023be512cd242247bae042e98556e71785c
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2021-05-31 06:24:25 -0400

    Create private_constants.js

---
+++ server/modules/private_constants.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.recaptcha_priv_key = 'YOUR_GOOGLE_RECAPTCHA_PRIVATE_KEY';
+exports.password_private_suffix = 'LONG_RANDOM_STRING1';
+exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
+exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+if (exports.password_private_suffix == 'LONG_RANDOM_STRING1') throw new Error('You MUST change default value exports.password_private_suffix !')
+
+exports.walletspassphrase = {
+    'DOGE' : 'LONG_RANDOM_STRING3',
+    'FREAK' : 'LONG_RANDOM_STRING4'
+};
+
+exports.IsUnlimitedAddress = function(addr)
+{
+    if (!addr)
+        return false;
+        
+    if (addr.indexOf("127.0.0.1") < 0)
+        return false;
+        
+    return true;
+}
